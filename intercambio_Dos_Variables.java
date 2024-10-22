eab7639207f6d1c974f798fe88cb757af5af2345
Chad154 <chad154154@gmail.com>
2024-10-22 09:47:56 +0200

    hola

---
+++ intercambio_Dos_Variables.java
@@ -0,0 +1,25 @@
+public class intercambio_Dos_Variables {
+
+    public static void main(String[] args) {
+        int x,y,c;
+
+        x = 1;
+
+        y = 5;
+
+        System.out.println("Las variables inicales son x = "+x+" y = "+y); 
+
+        c = x;
+        x = y;
+        y = c;
+
+        System.out.println("Las variables cambidas son: x = "+x+" y = "+y);
+
+        
+        
+
+
+        
+    }
+    
+}
---
+++ tipo_Entero.java
@@ -0,0 +1,16 @@
+public class tipo_Entero {
+    public static void main(String[] args) {
+        int numero1, numero2;
+        double numero_Decimal1, numero_Decimal2;
+        numero1 = 2;
+        numero2 = 5;
+        numero_Decimal1 = 15.23;
+        numero_Decimal2 = 17.345678;
+
+        System.out.println(numero1+" "+numero2+" "+numero_Decimal1+" "+numero_Decimal2);
+        System.out.println("La suma es = "+(numero1+numero2));
+        System.out.println("La suma es = "+(numero_Decimal1+numero_Decimal2));
+        
+    }
+    
+}
