e8220cc5f5ab55880fc77c1ed419e0ec237a7ccc
Chad154 <chad154154@gmail.com>
2024-10-25 13:57:30 +0200

    Utilizacion de char.at

---
+++ cadenaLenght.java
@@ -0,0 +1,19 @@
+public class cadenaLenght {
+
+    public static void main(String[] args) {
+
+        String cadena = "Curso 2024 - 2025";
+        int longitud = cadena.length();
+
+        // .lenght Devuelve la cantidad de caracteres que hay en una cadena
+        System.out.println(longitud);
+        System.out.println(cadena.length());
+
+        // .charAt Devuelve el caracter que esta en esa posicion
+        System.out.println(cadena.charAt(0));
+
+        //Sacar la letra final de la cadena
+
+        System.out.println("La letra final de la cadena es: " + cadena.charAt(longitud-1));
+    }
+}
