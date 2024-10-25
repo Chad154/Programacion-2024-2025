b6a74beebacb5d75e0e07bca19a9daf3e73bff61
Chad154 <chad154154@gmail.com>
2024-10-25 13:45:40 +0200

    Programa mayor de edad

---
+++ mayorEdad.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class mayorEdad {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduzca su edad: ");
+        int edad = teclado.nextInt();
+
+        if (edad >= 18) {
+            System.out.println("Escribir usted es mayor de edad");
+        } else {
+            System.out.println("Usted es menor de edad");
+        }
+    }
+}
--- millasMarinas.java
+++ millasMarinas.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class millasMarinas {
 
     public static void main(String[] args) {
-        
+
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Introduzca el numero de millas a cambiar a metros: ");
@@ -12,5 +12,5 @@ public class millasMarinas {
         System.out.println("Las millas marinas convertidas a metros son: " + (millas * 1852) + " m");
         teclado.close();
     }
-    
+
 }
