8a6638d10891ec817ca644581952b023cd7c22c9
Chad154 <chad154154@gmail.com>
2024-10-25 13:40:31 +0200

    Programa Millas marinas

---
+++ millasMarinas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class millasMarinas {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduzca el numero de millas a cambiar a metros: ");
+        int millas = teclado.nextInt();
+
+        System.out.println("Las millas marinas convertidas a metros son: " + (millas * 1852) + " m");
+        teclado.close();
+    }
+    
+}
