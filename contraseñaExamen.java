008222eb9100141fca054e7100ff45f0414cb9ae
Chad154 <chad154154@gmail.com>
2024-11-04 13:25:38 +0100

    Sin Break Contraseña Examen

--- contraseñaExamen.java
+++ contraseñaExamen.java
@@ -3,27 +3,31 @@ import java.util.Scanner;
 public class contraseñaExamen {
 
     public static void main(String[] args) {
-
         Scanner teclado = new Scanner(System.in);
 
         String contraseña;
         int contador = 0;
 
-        do {
-
+        while (contador < 3) {
             System.out.println("Escriba la contraseña");
             contraseña = teclado.nextLine();
+
             if (contraseña.equals("PAQUITO")) {
+
                 System.out.println("ENHORABUENA, HAS ACCEDIDO AL SISTEMA");
-                break;
-            } else if (contador == 3) {
-                System.out.println("LO SIENTO, CUENTA BLOQUEADA");
-                break;
+                contador = 4;
+
             } else {
+
                 System.out.println("Contraseña Incorrecta");
                 contador++;
             }
-        } while (true);
+
+            if (contador == 3) {
+
+                System.out.println("LO SIENTO, CUENTA BLOQUEADA");
+            }
+        }
 
         teclado.close();
     }
