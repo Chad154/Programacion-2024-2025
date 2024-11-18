1ed27c8a1cb96d4c959d6d0421141b5ebd474283
Chad154 <chad154154@gmail.com>
2024-11-18 13:53:38 +0100

    Triangulo dibujado Invertido pidiendo tambien el caracter a dibujar

---
+++ tema2_programacion/trianguloDibujadoReves.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class trianguloDibujadoReves {
+
+    public static void main(String args []){
+
+        Scanner teclado = new Scanner(System.in);
+
+        int altura;
+        char caracater;
+
+        System.out.println("Introduzca la altura");
+        altura = teclado.nextInt();
+        System.out.println("Introduzca el caracater que quiere utlizar");
+        caracater = teclado.next().charAt(0);
+        System.out.println("");
+
+        for(int i = altura; i >= 1; i-- ){
+            for(int j = 1; j <= i;j++){
+                System.out.print(caracater);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
