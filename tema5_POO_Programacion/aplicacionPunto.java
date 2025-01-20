f6d8c114b7da28e37ca204242dec0f45f92a279f
Chad154 <chad154154@gmail.com>
2025-01-20 14:07:11 +0100

    Modificacion con mejoras en el codigo

--- tema5_POO_Programacion/aplicacionPunto.java
+++ tema5_POO_Programacion/aplicacionPunto.java
@@ -3,21 +3,20 @@ package tema5_POO_Programacion;
 public class aplicacionPunto {
 
     public static void main(String[] args) {
-        
+
         punto p1 = new punto();
         p1.mostrarPunto();
 
         p1.setX(5);
         p1.setY(5);
-        p1.calcularDistancia();
+        System.out.println("La distancia de p1 es: " + p1.calcularDistancia());
         p1.mostrarPunto();
 
         p1.setX(4);
         p1.setY(3);
-        p1.calcularDistancia();
+        System.out.println("La distancia de p1 es: " + p1.calcularDistancia());
         p1.mostrarPunto();
 
-        
     }
 
 }
--- tema5_POO_Programacion/punto.java
+++ tema5_POO_Programacion/punto.java
@@ -2,20 +2,12 @@ package tema5_POO_Programacion;
 
 public class punto {
 
-    private double distancia, x, y;
+    private double x, y;
 
     public punto() {
         x = 0;
         y = 0;
-        distancia = 0;
-    }
 
-    public double getDistancia() {
-        return distancia;
-    }
-
-    public void setDistancia(double distancia) {
-        this.distancia = distancia;
     }
 
     public double getX() {
@@ -34,18 +26,16 @@ public class punto {
         this.y = y;
     }
 
-   
-
     public void mostrarPunto() {
 
         System.out.println("Cordenada X: " + x);
         System.out.println("Coordenada Y: " + y);
-        System.out.println("La distancia entre el centro y su punto es: " + distancia);
         System.out.println();
     }
 
-    public void calcularDistancia() {
-        distancia = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    public double calcularDistancia() {
+
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
 }
