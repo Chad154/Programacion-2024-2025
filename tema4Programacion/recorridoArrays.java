efb710a358b45af945f51ec94697382edb4e492c
Chad154 <chad154154@gmail.com>
2025-01-27 13:53:22 +0100

    Una clase array de repaso

---
+++ tema4Programacion/recorridoArrays.java
@@ -0,0 +1,42 @@
+
+import java.util.Arrays;
+
+public class recorridoArrays {
+
+    public static void main(String[] args) {
+        int[] array = { 1, 4, 5, 6, 7, 8, 3, 8 };
+        // int [][] matriz = {{1,2}, {3,4,5},{0}}
+
+        System.out.println("El array principal es: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+
+        System.out.println("------------------------");
+        cambiarArray(array, 10);
+        System.out.println("El array despues de la funcion es: ");
+        for (int elemento : array) {
+            System.out.println(elemento + " ");
+        }
+
+        System.out.println("Imprimimos el array con Arrays.toString");
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void imprimir(int[] vector) {
+        // Lo mostramos
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void cambiarArray(int[] vector, int num) {
+        // Lo incrementamos con num
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] += num;
+        }
+    }
+
+}
